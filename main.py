c78e839ea5dbe0d0768400d62ac6eeabdcd6a0ed
CodinWithSkanda1 <74732131+CodinWithSkanda1@users.noreply.github.com>
2020-12-05 12:19:08 -0500

    Created main.py for tutorial 1

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame, sys
+
+screen = pygame.display.set_mode((500, 500))
+
+while True:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      pygame.quit()
+      quit("User closed the window...")
+      sys.exit()
