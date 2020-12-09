72c893b0ac538df8ce17a69de01489f77d417554
CodinWithSkanda1 <74732131+CodinWithSkanda1@users.noreply.github.com>
2020-12-09 10:34:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ while True:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       pygame.quit()
-      quit("User closed the window...")
       sys.exit()
       
     pygame.display.update()
